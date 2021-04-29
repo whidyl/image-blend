2ec35576d982412b2a353a5ee64b99163d8e3cbe
whidyl <whidyldev@gmail.com>
2021-04-28 22:15:19 -0600

    readability refactor, reduced redundand tailwind

--- src/components/Mash/Mash.js
+++ src/components/Mash/Mash.js
@@ -1,27 +1,44 @@
-import React, { useEffect } from 'react';
-import ImageLayer from './ImageLayer';
-import axios from 'axios';
-import '../../animations.css';
-
+import React, { useEffect } from "react";
+import ImageLayer from "./ImageLayer";
+import "../../animations.css";
 
 const Mash = () => {
-    const urls = []
+  const urls = [];
 
-    // useEffect(() => {
-    //     const fetchImage = async () => {
-    //         const response = await axios.get("https://source.unsplash.com/random");
-    //         console.log(response.request.responseURL);
-    //     }
-    //     fetchImage();
-    // }, [])
+  // useEffect(() => {
+  //     const fetchImage = async () => {
+  //         const response = await axios.get("https://source.unsplash.com/random");
+  //         console.log(response.request.responseURL);
+  //     }
+  //     fetchImage();
+  // }, [])
 
-    return (
-        <div style={{ width: "500px", height: "500px", position: "absolute", display: "inline-block"}}>
-            <ImageLayer url="https://source.unsplash.com/random/?anime" blendMode="normal" opacity="100%"/>
-            <ImageLayer url="https://source.unsplash.com/random/?goth" blendMode="color-dodge" animate=""/>
-            <ImageLayer url="https://source.unsplash.com/random/?death" blendMode="overlay" opacity="100%"/>
-        </div>
-    );
-}
+  return (
+    <div
+      style={{
+        width: "500px",
+        height: "500px",
+        position: "absolute",
+        display: "inline-block",
+      }}
+    >
+      <ImageLayer
+        url="https://source.unsplash.com/random/?anime"
+        blendMode="normal"
+        opacity="100%"
+      />
+      <ImageLayer
+        url="https://source.unsplash.com/random/?goth"
+        blendMode="color-dodge"
+        animate=""
+      />
+      <ImageLayer
+        url="https://source.unsplash.com/random/?death"
+        blendMode="overlay"
+        opacity="100%"
+      />
+    </div>
+  );
+};
 
 export default Mash;
