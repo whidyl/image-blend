6bf30e02f4477b5b8ef255811654006ddbda4149
whidyl <whidyldev@gmail.com>
2021-04-30 14:16:58 -0600

    refresh button

--- src/components/Editor/LayersPanel/ImgSearchBox.tsx
+++ src/components/Editor/LayersPanel/ImgSearchBox.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 interface Props {
   query: string;
@@ -7,13 +7,20 @@ interface Props {
 }
 
 const ImgSearchBox: React.FC<Props> = ({ query, setQuery, refreshURL }) => {
-  useEffect(() => {
-      const timeoutID = setTimeout(refreshURL, 1000)
-      return () => {
-          clearTimeout(timeoutID);
-      }
-  }, [query])
+  const [disableRefresh, setDisableRefresh] = useState(false);
 
+  useEffect(() => {
+    const timeoutID = setTimeout(refreshURL, 1000);
+    return () => {
+      clearTimeout(timeoutID);
+    };
+  }, [query]);
+
+  const onRefreshClick = () => {
+    setDisableRefresh(true);
+    setTimeout(() => setDisableRefresh(false), 2000);
+    refreshURL();
+  };
 
   return (
     <span className="mb-1 mt-2 flex items-center">
@@ -25,10 +32,18 @@ const ImgSearchBox: React.FC<Props> = ({ query, setQuery, refreshURL }) => {
         placeholder="Image search query"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className=" mr-2 px-1 pl-2 py-2 basic-clickable rounded-md bg-muidark-2 shadow-inner flex-grow placeholder-white placeholder-opacity-30"
+        className=" mr-2 px-1 pl-2 py-2 w-9 basic-clickable rounded-md bg-muidark-2 shadow-inner flex-grow placeholder-white placeholder-opacity-30"
       />
-      <button>
-          refresh
+      <button
+        className={`mr-1 p-2 w-8 h-8 bg-muidark-2 basic-clickable disabled:opacity-50 ${ disableRefresh ? "cursor-default" : "" }`}
+        disabled={disableRefresh}
+        onClick={() => onRefreshClick()}
+      >
+        <img
+          className={`opacity-50 ${ disableRefresh ? "" : "hover:opacity-100 cursor-pointer " } `}
+          src="icons8-refresh-30.png"
+          alt="expand layer"
+        />
       </button>
     </span>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -32,7 +32,9 @@ module.exports = {
     },
   },
   variants: {
-    extend: {},
+    extend: {
+      opacity: ['disabled'],
+    },
   },
   plugins: [],
 }
